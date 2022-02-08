a93e09f4e48e552736ed4979bbae8b4880b6e567
tore <tore@amundsen.org>
2022-02-08 21:56:52 +0100

    Added diagnostics #25

---
+++ custom_components/amshan/diagnostics.py
@@ -0,0 +1,15 @@
+"""AMSHAN diagnostics."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    diagnostics = {"config_entry": config_entry.as_dict()}
+    return diagnostics
